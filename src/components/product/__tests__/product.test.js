a982f502ea551f59c318217746b248107f6b8d71
LL Powe <powe2009@gmail.com>
2023-06-01 11:06:07 -0400

    abc

--- src/components/product/__tests__/product.test.js
+++ src/components/product/__tests__/product.test.js
@@ -31,7 +31,7 @@ describe('Add to cart tests', () => {
         expect(cartItems[0]).toEqual(mockProduct);
     });
 
-    test('Add 1 product when cart has 1 item', async() => {
+    test('Add 1 product when cart has 1 item of the same Product', async() => {
         const mockProduct = {
             id: 1, name: 'P 1',
             imageUrl: 'p1.gif',
@@ -56,5 +56,32 @@ describe('Add to cart tests', () => {
         const cartItems = store.getState().cart.cartItems;
         expect(cartItems.length).toEqual(1);
         expect(cartItems[0].quantity).toEqual(2);
+    });
+
+    test('Add 1 product when cart has 1 item of a different Product', async() => {
+        const mockProduct = {
+            id: 2,
+            name: 'P 2',
+            imageUrl: 'p2.gif',
+            price: 20,
+            quantity: 1
+        }
+        const { store } = renderWithProviders(
+            <Product product={mockProduct} />,
+            {
+                preloadedState: {
+                    cart: {
+                        cartItems: [
+                            { id: 1, name: 'P 1', imageUrl: 'p1.gif', price: 10, quantity: 1 },
+                        ],
+                        isCartOpen: false
+                    }
+                }
+            }
+        )
+        const buttonEl = screen.queryByText(/add to cart/i);
+        await fireEvent.click(buttonEl);
+        const cartItems = store.getState().cart.cartItems;
+        expect(cartItems.length).toEqual(2);
     });
 });
